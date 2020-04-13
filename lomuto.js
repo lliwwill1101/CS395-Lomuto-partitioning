852e82ac88df2f41859754d33829a2c76250504c
Graeme Holliday <shadowsofundin@gmail.com>
2020-04-13 14:50:20 -0700

    fun stuffs

--- lomuto.js
+++ lomuto.js
@@ -1,6 +1,39 @@
+var started = false;
+
 function welcome() {
-   var arr = document.getElementsByTagName("input");
+   sayIt(document.getElementById("desc").innerText);
+   var arr = document.getElementsByName("cell");
    for(var i = 0; i < arr.length; i++) {
       arr[i].value = getRandomInt(99);
    }
+}
+
+function clear() {
+   var arr = document.getElementsByName("cell");
+   for(var i = 0; i < arr.length; i++) {
+      arr[i].setAttribute("class", "unhighlighed");
+   }
+}
+
+function step() {
+   var arr = document.getElementsByName("cell");
+   if(!started) {
+      started = true;
+      arr[arr.length - 1].setAttribute("class", "pivot");
+   } else {
+      clear();
+   }
+}
+
+function lomuto(arr, lo, hi) {
+   /*
+   partition(arr[], lo, hi) 
+    pivot = arr[hi]
+    i = lo     // place for swapping
+    for j := lo to hi – 1 do
+        if arr[j] <= pivot then
+            swap arr[i] with arr[j]
+            i = i + 1
+    swap arr[i] with arr[hi]
+    return i*/
 }
